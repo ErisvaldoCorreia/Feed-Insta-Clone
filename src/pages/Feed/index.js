ca981934c5bd729b3e8abbdd75b8a2ba3714b081
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-10-02 01:32:26 -0300

    feat: adiciona feed pt1

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,31 +1,79 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect} from 'react';
+import { FlatList, View, Image } from 'react-native';
+
+import api from '../../../services/service';
+
+import {
+  Container,
+  Post,
+  Header,
+  Avatar,
+  Name,
+  Description,
+  Loading,
+} from './styles';
 
 export default function Feed() {
+  const [feed, setFeed] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  async function loadPage() {
+    if (loading) return;
+
+    setLoading(true);
+
+    const { data } = await api.get(`/feed?_expand=author`);
+
+    setLoading(false);
+
+    setFeed(data);
+  }
+
+  async function refreshList() {
+    setRefreshing(true);
+
+    await loadPage();
+
+    setRefreshing(false);
+  }
+
+  useEffect(() => {
+    loadPage();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>FEED INSTA CLONE</Text>
-      <Text style={styles.sub}>Usando React Native</Text>
-    </View>
+    <Container>
+      <FlatList
+        key="list"
+        data={feed}
+        keyExtractor={(item) => String(item.id)}
+        viewabilityConfig={{
+          viewAreaCoveragePercentThreshold: 10,
+        }}
+        showsVerticalScrollIndicator={false}
+        onRefresh={refreshList}
+        refreshing={refreshing}
+        ListFooterComponent={loading && <Loading />}
+        renderItem={({ item }) => (
+          <Post>
+            <Header>
+              <Avatar source={{ uri: item.author.avatar }} />
+              <Name>{item.author.name}</Name>
+            </Header>
+
+            {/* <FeedImage
+              aspectRatio={item.aspectRatio}
+              smallSource={{ uri: item.image }}
+              source={{ uri: item.small }}
+            /> */}
+
+            <Description>
+              <Name>{item.author.name}</Name> {item.description}
+            </Description>
+          </Post>
+        )}
+      />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    color: '#111',
-    fontWeight: 'bold'
-  },
-  sub: {
-    fontWeight: '200',
-    fontSize: 15,
-    color: '#444',
-    marginTop: 5,
-  }
-});
